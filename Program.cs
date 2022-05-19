9debb8fe88c40ee393e3ee60ad9e49d03335b621
Mateus Fonseca Lima <mateus.fonseca@fitbank.com.br>
2022-05-19 17:22:21 -0300

    final implementation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExampleException.Entities;
+using ExampleException.Entities.Exceptions;
+
+try
+{
+    Console.Clear();
+
+    Console.WriteLine("Enter account data: \n");
+
+    Console.Write("Number: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.Write("Holder: ");
+    string holder = Console.ReadLine();
+    Console.Write("Initial balance: ");
+    double initialBalance = double.Parse(Console.ReadLine());
+    Console.Write("Withdraw limit: ");
+    double withdrawLimit = double.Parse(Console.ReadLine());
+
+    Account a = new Account(number, holder, initialBalance, withdrawLimit);
+
+
+    // ----------------------------------------------------------- Withdraw
+
+    Console.Clear();
+
+    Console.WriteLine("Enter amount for withdraw: ");
+    double amountForWithdraw = double.Parse(Console.ReadLine());
+
+    a.Withdraw(amountForWithdraw);
+}
+catch (WithdrawException e)
+{
+    Console.WriteLine("\nWithdraw error: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("\nGeneric error: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("\nExecutes regardless of whether or not an exception occurs.\n");
+}
